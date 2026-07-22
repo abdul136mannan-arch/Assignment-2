6c665a06148343eea89ee7e0e8ae216c58d21360
abdul136mannan-arch <abdul136mannan@gmail.com>
2026-07-22 18:28:39 +0530

    Update backup_script.sh

--- Question 1/backup_script.sh
+++ Question 1/backup_script.sh
@@ -1,1 +1,37 @@
+#!/bin/bash
 
+SOURCE_DIR="submissions"
+BACKUP_DIR="backup"
+REPORT="report.txt"
+ERROR_LOG="error.log"
+
+mkdir -p "$BACKUP_DIR"
+
+processed=0
+duplicates=0
+backedup=0
+
+declare -A hashes
+
+for file in "$SOURCE_DIR"/*
+do
+    if [ -f "$file" ]; then
+        ((processed++))
+
+        hash=$(sha256sum "$file" 2>>"$ERROR_LOG" | awk '{print $1}')
+
+        if [[ -n "${hashes[$hash]}" ]]; then
+            ((duplicates++))
+        else
+            hashes[$hash]=1
+            cp "$file" "$BACKUP_DIR/" 2>>"$ERROR_LOG"
+            ((backedup++))
+        fi
+    fi
+done
+
+echo "Files Processed : $processed" > "$REPORT"
+echo "Duplicate Files : $duplicates" >> "$REPORT"
+echo "Files Backed Up : $backedup" >> "$REPORT"
+
+echo "Backup Completed."
